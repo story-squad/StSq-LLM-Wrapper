c3aba97e5bc9916029f958600916d33d9a90ba13
Tasha Upchurch <TashaSkyUp@gmail.com>
2022-07-11 15:33:19 -0400

    feature for authenticating with open ai, fixes test_create_wrapper

--- src/StSqLLMWrapper/llmwrapper.py
+++ src/StSqLLMWrapper/llmwrapper.py
@@ -1,5 +1,12 @@
 class LLMWrapper:
-    def __init__(self, llm_path):
-        pass
+    def __init__(self, API_NAME, API_KEY=None):
+        import os
+        import openai
+        if API_NAME.lower() == "openai":
+            if not API_KEY:
+                openai.api_key = os.getenv("OPENAI_API_KEY")
+            self.models = openai.Model.list()
+            self.authenticated = True
+
 
 
