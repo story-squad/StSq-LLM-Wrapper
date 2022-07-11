28284e78f1c35548a9d3eb6ffa40f9d1b988a19e
Tasha Upchurch <TashaSkyUp@gmail.com>
2022-07-11 13:58:42 -0400

    initial commit and project structure

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+   name='StSqLLMWrapper',
+   version='0.0.1',
+   description="""An interface that wraps LLM's APIs""",
+   author='Tasha Upchurch, Jeannine Proctor, Darwin Johnson, ',
+   author_email='TashaSkyUp@gmail.com',
+   packages=['StSqLLMWrapper'],
+   install_requires=[], #external packages as dependencies
+   scripts=[]
+)
