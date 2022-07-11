cc42d4600cfc28b536c5a3089d3adf8a6dac404a
Tasha Upchurch <TashaSkyUp@gmail.com>
2022-07-11 19:23:59 -0400

    features to support specific kwargs for completion api for openai

--- src/StSqLLMWrapper/llmwrapper.py
+++ src/StSqLLMWrapper/llmwrapper.py
@@ -6,6 +6,23 @@ class LLMDefaults():
     default_completion_model_name: str = "NONE"
 
 
+@dataclasses.dataclass
+class KWArgs_class:
+    API_NAME: str = "NONE"
+
+
+@dataclasses.dataclass
+class OpenaiKWArgs(KWArgs_class):
+    API_NAME: str = "openai"
+    temperature: int = 50,
+    max_tokens: int = 40,
+    top_p: float = .7,
+    best_of: int = 1,
+    frequency_penalty: float = .2,
+    presence_penalty: float = 0,
+    stop: str = "."
+
+
 class LLMWrapper:
     def __init__(self, API_NAME, API_KEY=None):
         import os
@@ -23,8 +40,15 @@ class LLMWrapper:
         else:
             raise Exception("Invalid API name")
 
-    def completion(self, text):
+    def completion(self, prompt="test", kwargsclass: KWArgs_class = {}):
         if self.is_openai_api:
             import openai
-            result = openai.Completion.create(model=self.model_defaults.default_completion_model_name, prompt=text)
+            kwargs_dict= {}
+            if issubclass(kwargsclass.__class__,KWArgs_class) :
+                if kwargsclass.API_NAME.lower() != "openai":
+                    raise Exception("keyword args class not for use with openai api")
+                kwargs_dict = kwargsclass.__dict__
+                kwargs_dict.pop("API_NAME")
+            result = openai.Completion.create(model=self.model_defaults.default_completion_model_name, prompt=prompt,
+                                              **kwargs_dict)
             return result.choices[0].text
