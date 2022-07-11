f922e7ca8afbb384108379b8634fdb59d05b67d1
Tasha Upchurch <TashaSkyUp@gmail.com>
2022-07-11 16:01:34 -0400

    added test and feature for reporting invalid api key

--- tests/test_openai_provider.py
+++ tests/test_openai_provider.py
@@ -14,8 +14,16 @@ def test_create_wrapper():
     assert my_llm.authenticated
     assert type(my_llm.models) is list
 
+
 def test_create_wrapper_with_invalid_api_name():
     from src import LLMWrapper
     with pytest.raises(Exception) as e_info:
         LLMWrapper("invalid_api_name")
     assert "Invalid API name" in str(e_info.value)
+
+
+def test_create_wrapper_with_invalid_api_key():
+    from src import LLMWrapper
+    with pytest.raises(Exception) as e_info:
+        LLMWrapper("openai", "invalid_api_key")
+    assert "Invalid API key" in str(e_info.value) or "Incorrect API key" in str(e_info.value)
