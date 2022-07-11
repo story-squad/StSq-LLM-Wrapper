e8128cd0128741059284430da1ec8563bb21e197
Tasha Upchurch <TashaSkyUp@gmail.com>
2022-07-11 19:23:09 -0400

    2 tests for specific kwargs for completion api

--- tests/test_openai_provider.py
+++ tests/test_openai_provider.py
@@ -36,3 +36,29 @@ def test_default_completion_call():
     result = my_llm.completion("hello how are you?")
     assert type(result) is str
 
+
+def test_completion_with_specific_api_kwargs():
+    from src import LLMWrapper
+    from src import OpenaiKWArgs
+    kws = OpenaiKWArgs(temperature=1,
+                       max_tokens=40,
+                       top_p=.7,
+                       best_of=1,
+                       frequency_penalty=.2,
+                       presence_penalty=0,
+                       stop=".")
+    API_KEY = os.getenv("OPENAI_API_KEY")
+    my_llm = LLMWrapper("openai", API_KEY)
+    result = my_llm.completion(prompt="hello how are you?", kwargsclass=kws)
+    assert type(result) is str
+
+
+def test_completion_with_invalid_api_kwargs():
+    from src import LLMWrapper
+    from src import KWArgs_class
+    kws = KWArgs_class()
+    API_KEY = os.getenv("OPENAI_API_KEY")
+    my_llm = LLMWrapper("openai", API_KEY)
+    with pytest.raises(Exception) as e_info:
+        result = my_llm.completion(prompt="hello how are you?", kwargsclass=kws)
+    assert "keyword args class not for use with openai api" in str(e_info)
