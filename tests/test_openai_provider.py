820839c58b262f548b5d0c3c33ade6a6b461da04
Tasha Upchurch <TashaSkyUp@gmail.com>
2022-07-11 15:47:59 -0400

    added test for llm wrapper retrieving models list and testing for creating the wrapper while specifying the api key or not (and expecting the wrapper to get it from the os)

--- tests/test_openai_provider.py
+++ tests/test_openai_provider.py
@@ -4,7 +4,12 @@ import os
 
 def test_create_wrapper():
     from src import LLMWrapper
-    API_KEY = os.getenv("STORYSQUADAI_API_KEY")
+    API_KEY = os.getenv("OPENAI_API_KEY")
     my_llm = LLMWrapper("openai", API_KEY)
     assert my_llm.API_KEY == API_KEY
     assert my_llm.authenticated
+    assert type(my_llm.models) is list
+    my_llm = LLMWrapper("openai")
+    assert my_llm.API_KEY == API_KEY
+    assert my_llm.authenticated
+    assert type(my_llm.models) is list
