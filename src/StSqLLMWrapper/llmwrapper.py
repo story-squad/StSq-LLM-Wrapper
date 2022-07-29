0a40a4d3b5f2360bac1df83fba6a46edf9961579
Tasha Upchurch <TashaSkyUp@gmail.com>
2022-07-28 21:39:06 -0400

    feature module for handling different context document loaders for personalities. feature module for exception types feature module for filters using LLMWrappers LLMProcessor base class

--- src/StSqLLMWrapper/llmwrapper.py
+++ src/StSqLLMWrapper/llmwrapper.py
@@ -10,6 +10,7 @@ class LLMResponse:
     """
     raw_response: 'typing.Any' = object()
     completion: str = None
+    completion_processed_data: dict = None
 
 
 @dataclasses.dataclass
@@ -21,13 +22,16 @@ class LLMDefaults:
 
 class OpenaiLLMDefaults():
     default_completion_model_name: str = "text-ada-001"
-    default_search_query_model_name: str = "text-search-curie-query-001"
-    default_search_document_model_name: str = "curie-search-document"
+    default_search_query_model_name: str = "text-search-babbage-query-001"
+    default_search_document_model_name: str = "babbage-search-document"
 
 
 @dataclasses.dataclass
 class LLMRequest:
-    API_NAME: str = None
+    """
+    main data interface to the LLMWrapper class. stores data that is sent to the LLM and
+    results from filters and pre- / post-processing.
+    """
     temperature: float = .5
     max_tokens: int = 40
     top_p: float = .7
@@ -38,12 +42,16 @@ class LLMRequest:
     prompt: str = None
     context: str = None
     documents: str = None
+    prompt_processed_data: dict = None
+    context_processed_data: dict = None
+    documents_processed_data: dict = None
     query: str = None
+    n: int = 1
 
 
 @dataclasses.dataclass
 class OpenaiKWArgs(LLMRequest):
-    API_NAME: str = "openai"
+    """KWArgs suitable for OPENAI"""
     temperature: float = .5
     max_tokens: int = 40
     top_p: float = .7
@@ -52,9 +60,10 @@ class OpenaiKWArgs(LLMRequest):
     presence_penalty: float = 0
     stop: str = "."
     prompt: str = None
+    n: int = 1
 
 
-class LLMFilter:
+class LLMProcessor:
     """Superclass that all LLM filters should inherit from, subclasses should implement the apply() method"""
 
     def __init__(self, name: str = "unnamed filter"):
@@ -70,13 +79,7 @@ class LLMFilter:
         return self.apply(request, response)
 
     def apply(self, request: LLMRequest, response: LLMResponse):
-        raise NotImplementedError("LLMFilter.apply() not implemented")
-
-    def get_param_value(self, param_name, param_value):
-        if param_name in self.params_dict:
-            return self.params_dict[param_name].get_value(param_value)
-        else:
-            raise ValueError("LLMFilter.get_param_value() - Parameter {} not found".format(param_name))
+        raise NotImplementedError("LLMProcessor.apply() not implemented")
 
 class LLMWrapper:
     def __init__(self, api_name, api_key=None, completion_model_name=None, search_query_model_name=None,
@@ -133,7 +136,7 @@ class LLMWrapper:
             raise Exception("incoming class is not LLMRequest")
 
         if self.is_openai_api:
-            oai_kwargs = {"api_name": "openai"}
+            oai_kwargs = {}
 
             if request.top_p is not None:
                 oai_kwargs["top_p"] = request.top_p
@@ -145,6 +148,7 @@ class LLMWrapper:
             oai_kwargs["frequency_penalty"] = request.frequency_penalty
             oai_kwargs["presence_penalty"] = request.presence_penalty
             oai_kwargs["stop"] = request.stop
+            oai_kwargs["n"] = request.n
 
             if request.context:
                 oai_kwargs["prompt"] = request.context + request.prompt
@@ -191,56 +195,79 @@ class LLMWrapper:
             if not issubclass(request.__class__, LLMRequest):
                 raise Exception("Searches only possible with LLMRequest")
             else:
-                if not kwargs["api_name"].lower() == "openai":
-                    raise Exception("keyword args class not for use with openai api")
-                else:
-                    result = self.open_ai_search(request)
-
-                    return result
+                result = self.open_ai_search(request)
+                return result
 
         elif self.is_other:
             raise Exception("not implemented")
 
     def completion(self, prompt=None, kwargs: LLMRequest = None) -> LLMResponse:
-
-        # ensure that the prompt is included in the kwargs object for consistency and create a new kwargs object if
-        # one is not provided
-
-        if not prompt and not kwargs:
-            raise Exception("No kwargs provided")
-
-        if kwargs:
-            if issubclass(kwargs.__class__, LLMRequest) and (prompt is not None):
-                if kwargs.prompt is not None:
-                    raise Exception("Prompt already provided")
-                else:
-                    kwargs.prompt = prompt
-        else:
-            kwargs = LLMRequest(prompt=prompt)
-            prompt = None
+        """
+        returns the completion response from the llm api, used for multiple completions
+        :param prompt:
+        :param kwargs:
+        :return: array of string completions
+        """
+        kwargs = self.kwargs_check(kwargs, prompt)
 
         if self.is_openai_api:
+            if not issubclass(kwargs.__class__, LLMRequest):
+                raise Exception("keyword args class not for use with openai api")
             import openai
 
-            kwargs = self.handle_kwargs(kwargs)
+            kwargs_dict = self.handle_kwargs(kwargs)
 
-            # check for compatible kwargs
-            if issubclass(kwargs.__class__, LLMRequest):
-                if kwargs.API_NAME.lower() != "openai":
-                    raise Exception("keyword args class not for use with openai api")
+            #kwargs.pop("api_name")
 
-            # if kwargs is not compatible with the current api, return a compatible kwargs
-
-            kwargs.pop("api_name")
-
-            model_to_use = self.completion_model_name
-
-            result = openai.Completion.create(model=model_to_use,
-                                              **kwargs)
+            result = openai.Completion.create(model=self.completion_model_name,
+                                              **kwargs_dict)
             out_result = LLMResponse(raw_response=result,
-                                     completion=result["choices"][0]["text"])
+                                     completion=result["choices"])
 
             return out_result
 
         elif self.is_other:
             raise Exception("not implemented")
+
+    def moderation(self, request: LLMRequest) -> LLMResponse:
+        """
+        returns the moderation response from the llm api
+        :param request:
+        :return:
+        """
+        if self.is_openai_api:
+            import openai
+
+            if not issubclass(request.__class__, LLMRequest):
+                raise Exception("Moderation only possible with LLMRequest")
+            else:
+                result = openai.Moderation.create(input=request.query,
+                                                  model=self.search_query_model_name)
+                out_result = LLMResponse(raw_response=result,
+                                         moderation=result["moderation"])
+
+                return out_result
+
+        elif self.is_other:
+            raise Exception("not implemented")
+    def kwargs_check(self, kwargs, prompt):
+        if not prompt and not kwargs:
+            raise Exception("No kwargs provided")
+
+        if kwargs:
+            if issubclass(kwargs.__class__, LLMRequest):
+                if (prompt is not None) and (kwargs.prompt is not None):
+                    raise Exception("Prompt already provided")
+                elif prompt is not None:
+                    kwargs.prompt = prompt
+                    prompt = None
+                elif kwargs.prompt is not None:
+                    # prompt already set correctly
+                    pass
+        else:
+            kwargs = LLMRequest(prompt=prompt)
+            prompt = None
+
+        # check for compatible kwargs
+        return kwargs
+
